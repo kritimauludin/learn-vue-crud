0e74448e18dd32b0460eb1bb29f27743d30e6ca3
Kriti Mauludin <49935056+kritimauludin@users.noreply.github.com>
2022-08-17 10:06:35 +0700

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -7,13 +7,13 @@ import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDHqawFpvXauMjEKLYSaaoRg5dWDMLisXY",
-  authDomain: "learnvue-bf112.firebaseapp.com",
-  projectId: "learnvue-bf112",
-  storageBucket: "learnvue-bf112.appspot.com",
-  messagingSenderId: "884725399281",
-  appId: "1:884725399281:web:1a13feaa6ba0436cdf3350",
-  measurementId: "G-SPF3KC09QT"
+  apiKey: "your-api-key",
+  authDomain: "your-auth-key",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
